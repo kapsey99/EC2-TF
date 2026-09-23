05eb5fed7d1dbeb16731b4c1ad03228d7b6f2619
kyellapp <kyellapp@kapseys-MacBook-Pro.local>
2026-09-23 14:17:06 +0530

    msg

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 # create EC2 instance
 resource "aws_instance" "terraform_ec2"{
     ami = "ami-0fef201115eefe936"
     instance_type = "t3.micro"
     key_name = aws_key_pair.ec2-key.key_name
-    subnet_id = "subnet-0900a843e800330cf"
+    subnet_id     = data.aws_subnets.default.ids[0]
 
     tags = {
         Name = "EC2-TF"
@@ -25,6 +36,6 @@ resource "aws_key_pair" "ec2-key"{
 # store the private key locally 
 resource "local_file" "private-key"{
     content = tls_private_key.RSA.private_key_pem
-    filename = "/home/ec2-user/EC2-TF/ec2-key-pair.pem"
+    filename = "${path.module}/ec2-key-pair.pem"
     file_permission = "0400"
 }
