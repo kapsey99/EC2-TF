08cfa3e148019ab2193d60e7abc80bfd9f6895af
kyellapp <kyellapp@kapseys-MacBook-Pro.local>
2026-09-23 13:18:56 +0530

    msg

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "terraform_ec2"{
     ami = "ami-0fef201115eefe936"
     instance_type = "t3.micro"
     key_name = aws_key_pair.ec2-key.key_name
+    subnet = "subnet-0900a843e800330cf"
 
     tags = {
         Name = "EC2-TF"
