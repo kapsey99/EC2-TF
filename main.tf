92c8f11cbdf36f13104c4b5e3398a49b6c0274c3
kyellapp <kyellapp@kapseys-MacBook-Pro.local>
2026-09-23 13:15:33 +0530

    msg

--- main.tf
+++ main.tf
@@ -15,11 +15,11 @@ resource "tls_private_key" "RSA"{
 
 resource "aws_key_pair" "ec2-key"{
     key_name = "ec2-key-pair"
-    public_key = "tls_private_key.RSA.public_key_ossh"
+    public_key = "tls_private_key.RSA.public_key_openssh"
 }
 
 resource "local_file" "private-key"{
     content = tls_private_key.RSA.private_key_pem
-    filename = "/Users/kyellapp/ec2-key-pair.pem"
+    filename = "/home/ec2-user/EC2-TF/ec2-key-pair.pem"
     file_permission = "0440"
 }
